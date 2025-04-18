7e9a1e62317be6427c7af19c29c97bfeeca058a3
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-04-18 12:31:46 -0300

    refactor(mapper): remove unused transaction mapping methods

--- picpay-simplificado/src/main/java/com/desafio/picpay_simplificado/web/mapper/TransactionMapper.java
+++ picpay-simplificado/src/main/java/com/desafio/picpay_simplificado/web/mapper/TransactionMapper.java
@@ -6,7 +6,6 @@ import com.desafio.picpay_simplificado.web.dto.TransactionRequestDto;
 import com.desafio.picpay_simplificado.web.dto.TransactionResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -14,30 +13,9 @@ public interface TransactionMapper {
 
     TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(source = "amount", target = "amount")
-    @Mapping(source = "payerId", target = "payer.id")
-    @Mapping(source = "payeeId", target = "payee.id")
-    Transaction toTransaction(TransactionRequestDto requestDto);
-
     @Mapping(source = "payer.id", target = "payer")
     @Mapping(source = "payee.id", target = "payee")
     TransactionResponseDto toDto(Transaction transaction);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(source = "amount", target = "amount")
-    @Mapping(source = "payerId", target = "payer")
-    @Mapping(source = "payeeId", target = "payee")
-    void updateFromDto(TransactionRequestDto updateDto, @MappingTarget Transaction transaction);
-
-    default User map(Long id) {
-        if (id == null) return null;
-        User user = new User();
-        user.setId(id);
-        return user;
-    }
 
     default Transaction toEntity(TransactionRequestDto dto, User payer, User payee) {
         Transaction transaction = new Transaction();
