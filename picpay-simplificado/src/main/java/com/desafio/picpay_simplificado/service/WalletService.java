19478dec5d39a81d89a0bd09ab157f5cc9daf7ca
Isabel Henrique <contato.isabelhenrique@gmail.com>
2025-04-22 12:23:01 -0300

    feat: add logging in wallet service

--- picpay-simplificado/src/main/java/com/desafio/picpay_simplificado/service/WalletService.java
+++ picpay-simplificado/src/main/java/com/desafio/picpay_simplificado/service/WalletService.java
@@ -8,11 +8,13 @@ import com.desafio.picpay_simplificado.web.dto.WalletResponseDto;
 import com.desafio.picpay_simplificado.web.exception.WalletNotFoundException;
 import com.desafio.picpay_simplificado.web.mapper.WalletMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class WalletService {
@@ -21,23 +23,29 @@ public class WalletService {
 
     @Transactional(readOnly = true)
     public WalletResponseDto findByUser(Long userId) {
+        log.info("Searching for wallet by user ID: {}", userId);
         Wallet wallet = findWalletByUserId(userId);
         return WalletMapper.INSTANCE.toDto(wallet);
     }
 
     public void createWallet(User user) {
+        log.info("Creating wallet for user ID: {}", user.getId());
         Wallet wallet = new Wallet();
         wallet.setUser(user);
         wallet.setBalance(BigDecimal.ZERO);
         walletRepository.save(wallet);
+        log.info("Wallet successfully created for user ID: {}", user.getId());
     }
 
     @Transactional
     public WalletResponseDto depositToWallet(Long userId, WalletDepositDto depositDto) {
+        log.info("Depositing amount {} into wallet of user ID: {}", depositDto.amount(), userId);
         Wallet wallet = findWalletByUserId(userId);
         wallet.setBalance(wallet.getBalance().add(depositDto.amount()));
 
         Wallet updatedWallet = walletRepository.save(wallet);
+        log.info("Deposit successful. New balance for user ID {}: {}", userId, updatedWallet.getBalance());
+
         return WalletMapper.INSTANCE.toDto(updatedWallet);
     }
 
